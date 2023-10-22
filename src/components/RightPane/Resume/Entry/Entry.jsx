c224c8ce1e353ff857afd8e86d500f86962b0f51
Manav Chhuneja <manavchhuneja@Manavs-Mac-mini.local>
2023-10-21 23:22:19 -0400

    adding props to the entries component to accept dynamic data

--- src/components/RightPane/Resume/Entry/Entry.jsx
+++ src/components/RightPane/Resume/Entry/Entry.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import styles from "./Entry.module.css";
 
-function Entry() {
+function Entry({ title, company, startDate, endDate, description }) {
   return (
     <div className={styles["entry-container"]}>
-      <h3>Job Title - Company</h3>
-      <h4>Start Date - End Date</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea et similique
-        quibusdam, maiores incidunt, quae exercitationem earum soluta vel dolore
-        consectetur delectus voluptate velit. Commodi, doloremque ullam!
-        Necessitatibus, libero quidem. Ea ipsam facilis odit sit maiores id,
-        pariatur odio nobis sed perspiciatis tempore saepe consequuntur
-        quibusdam sunt quo officia consectetur architecto totam. Cumque modi
-        quisquam animi labore sapiente consequatur unde!
-      </p>
+      <h3>
+        {title} - {company}
+      </h3>
+      <h4>
+        {startDate} - {endDate}
+      </h4>
+      <p>{description}</p>
     </div>
   );
 }
